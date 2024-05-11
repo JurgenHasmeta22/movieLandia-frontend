bb319b17c56bce7a556a881df11fdb11728db552
JurgenHasmeta22 <jurgenhasmeta12@yahoo.com>
2024-05-11 15:31:02 +0200

    carousel bug fix

--- src/pages/home/homeCarousel/HomeCarousel.tsx
+++ src/pages/home/homeCarousel/HomeCarousel.tsx
@@ -1,25 +1,46 @@
-import { useState } from "react";
-import { IconButton, Box } from "@mui/material";
+import { useState, useEffect } from "react";
+import { Box, IconButton } from "@mui/material";
 import NavigateBeforeIcon from "@mui/icons-material/NavigateBefore";
 import NavigateNextIcon from "@mui/icons-material/NavigateNext";
 
 const Carousel = ({ images }: any) => {
-    const [index, setIndex] = useState(0);
+    const [startIndex, setStartIndex] = useState(0);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setStartIndex((prevIndex) => (prevIndex === images.length - 3 ? 0 : prevIndex + 1));
+        }, 3000);
+
+        return () => clearInterval(interval);
+    }, [images]);
 
     const handleNext = () => {
-        setIndex((prevIndex) => (prevIndex === images.length - 1 ? 0 : prevIndex + 1));
+        setStartIndex((prevIndex) => (prevIndex === images.length - 3 ? 0 : prevIndex + 1));
     };
 
     const handlePrev = () => {
-        setIndex((prevIndex) => (prevIndex === 0 ? images.length - 1 : prevIndex - 1));
+        setStartIndex((prevIndex) => (prevIndex === 0 ? images.length - 3 : prevIndex - 1));
     };
 
     return (
-        <Box display="flex" alignItems="center" justifyContent="center" overflow="hidden">
+        <Box
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+            overflow="hidden"
+            style={{ gap: "20px" }}
+        >
             <IconButton onClick={handlePrev}>
                 <NavigateBeforeIcon />
             </IconButton>
-            <img src={images[index].source} alt={`Slide ${index}`} />
+            {images.slice(startIndex, startIndex + 3).map((image: any, index: number) => (
+                <img
+                    key={index}
+                    src={image.source}
+                    alt={`Slide ${startIndex + index}`}
+                    style={{ marginRight: index === 2 ? 0 : "20px" }}
+                />
+            ))}
             <IconButton onClick={handleNext}>
                 <NavigateNextIcon />
             </IconButton>
