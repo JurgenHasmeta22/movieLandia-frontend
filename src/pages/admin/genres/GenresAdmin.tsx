9000946bac6b1376701dae8ccd8a0394cdaf171d
Jurgen Hasmeta <j.hasmeta.ext@teamsystem.com>
2024-05-12 16:07:34 +0200

    added reusable logic component for table admin

--- src/pages/admin/genres/GenresAdmin.tsx
+++ src/pages/admin/genres/GenresAdmin.tsx
@@ -1,39 +1,12 @@
-import { Box, Button, ListItemIcon, MenuItem, Typography } from "@mui/material";
-import {
-    MaterialReactTable,
-    useMaterialReactTable,
-    type MRT_ColumnDef,
-    MRT_GlobalFilterTextField,
-    MRT_ToggleFiltersButton,
-    MRT_ShowHideColumnsButton,
-    MRT_ToggleDensePaddingButton,
-    MRT_ColumnFiltersState,
-    MRT_PaginationState,
-    MRT_SortingState,
-} from "material-react-table";
+import { Box } from "@mui/material";
+import { MaterialReactTable, type MRT_ColumnDef } from "material-react-table";
 import HeaderDashboard from "~/components/admin/headerDashboard/HeaderDashboard";
-import { useState, useEffect, useMemo } from "react";
+import { useMemo } from "react";
 import { useNavigate } from "react-router-dom";
-import { Edit, Delete, Add } from "@mui/icons-material";
 import IGenre from "~/types/IGenre";
-import genreService from "~/services/api/genreService";
-import IGenreResponse from "~/types/IGenreResponse";
+import Table from "~/components/admin/table/Table";
 
 const GenresAdmin = () => {
-    const [rows, setRows] = useState<IGenre[]>([]);
-    const [rowsCount, setRowsCount] = useState<number>(0);
-    const [rowSelection, setRowSelection] = useState<any>({});
-    const [isError, setIsError] = useState(false);
-    const [isLoading, setIsLoading] = useState(false);
-    const [isRefetching, setIsRefetching] = useState(false);
-    const [columnFilters, setColumnFilters] = useState<MRT_ColumnFiltersState>([]);
-    const [globalFilter, setGlobalFilter] = useState("");
-    const [sorting, setSorting] = useState<MRT_SortingState>([]);
-    const [pagination, setPagination] = useState<MRT_PaginationState>({
-        pageIndex: 0,
-        pageSize: 5,
-    });
-
     const navigate = useNavigate();
     const columns = useMemo<MRT_ColumnDef<IGenre>[]>(
         () => [
@@ -50,178 +23,16 @@ const GenresAdmin = () => {
         navigate("/admin/genres/add");
     }
 
-    async function getGenres(): Promise<void> {
-        if (!rows.length) {
-            setIsLoading(true);
-        } else {
-            setIsRefetching(true);
-        }
-
-        try {
-            const response: IGenreResponse = await genreService.getGenres({});
-            setRows(response.rows);
-            setRowsCount(response.count);
-        } catch (error) {
-            setIsError(true);
-            console.error(error);
-            return;
-        }
-
-        setIsError(false);
-        setIsLoading(false);
-        setIsRefetching(false);
+    function handleDeleteGenre() {
+        navigate("/admin/genres/add");
     }
 
-    useEffect(() => {
-        getGenres();
-    }, [columnFilters, globalFilter, pagination.pageIndex, pagination.pageSize, sorting]);
-
-    // #region React Material Table logic
-    const table = useMaterialReactTable({
+    const { table } = Table({
         columns,
-        data: rows,
-        getRowId: (row) => String(row.id),
-        enableColumnOrdering: true,
-        enableRowSelection: true,
-        enablePagination: true,
-        enableRowActions: true,
-        enablePinning: true,
-        enableSortingRemoval: true,
-        enableColumnFilterModes: true,
-        enableColumnActions: true,
-        enableColumnFilters: true,
-        enableClickToCopy: true,
-        enableStickyHeader: true,
-        enableStickyFooter: true,
-        manualFiltering: true,
-        manualPagination: true,
-        manualSorting: true,
-        muiToolbarAlertBannerProps: isError
-            ? {
-                  color: "error",
-                  children: "Error loading data",
-              }
-            : undefined,
-        initialState: {
-            columnVisibility: { id: false },
-            showColumnFilters: false,
-            showGlobalFilter: true,
-            showLoadingOverlay: false,
-            density: "compact",
-            columnPinning: {
-                left: ["mrt-row-expand", "mrt-row-select"],
-                right: ["mrt-row-actions"],
-            },
-            columnFilters,
-            globalFilter,
-            isLoading,
-            pagination,
-            showAlertBanner: isError,
-            showProgressBars: isRefetching,
-            sorting,
-        },
-        onColumnFiltersChange: setColumnFilters,
-        onGlobalFilterChange: setGlobalFilter,
-        onPaginationChange: setPagination,
-        onSortingChange: setSorting,
-        onRowSelectionChange: setRowSelection,
-        state: { rowSelection },
-        paginationDisplayMode: "pages",
-        positionToolbarAlertBanner: "bottom",
-        muiSearchTextFieldProps: {
-            size: "small",
-            variant: "outlined",
-        },
-        muiTablePaperProps: {
-            style: {
-                padding: 18,
-            },
-        },
-        muiPaginationProps: {
-            color: "secondary",
-            rowsPerPageOptions: [5, 10, 15, 20],
-            shape: "rounded",
-            size: "small",
-            variant: "outlined",
-            style: {
-                paddingTop: 6,
-            },
-        },
-        renderRowActionMenuItems: ({ closeMenu, row }) => [
-            <MenuItem
-                key={0}
-                onClick={() => {
-                    navigate(`/admin/genres/${row.original.id}`, {
-                        state: {
-                            userId: row.original.id,
-                            from: "Genres",
-                        },
-                    });
-
-                    closeMenu();
-                }}
-                sx={{ m: 0 }}
-            >
-                <ListItemIcon>
-                    <Edit />
-                </ListItemIcon>
-                <Typography>Edit</Typography>
-            </MenuItem>,
-            <MenuItem
-                key={1}
-                onClick={async () => {
-                    closeMenu();
-                }}
-                sx={{ m: 0 }}
-            >
-                <ListItemIcon>
-                    <Delete />
-                </ListItemIcon>
-                <Typography>Delete</Typography>
-            </MenuItem>,
-        ],
-        renderTopToolbar: ({ table }) => {
-            return (
-                <Box
-                    sx={() => ({
-                        display: "flex",
-                        gap: "1rem",
-                        p: "10px",
-                        justifyContent: "space-between",
-                    })}
-                >
-                    <Box sx={{ display: "flex", gap: "1rem", alignItems: "center" }}>
-                        <MRT_GlobalFilterTextField table={table} />
-                        <MRT_ToggleFiltersButton table={table} />
-                        <MRT_ShowHideColumnsButton table={table} />
-                        <MRT_ToggleDensePaddingButton table={table} />
-                    </Box>
-                    <Box
-                        sx={{
-                            display: "flex",
-                            placeItems: "center",
-                            placeContent: "center",
-                            gap: "1rem",
-                        }}
-                    >
-                        <Button color="success" onClick={handleAddGenre} variant="contained">
-                            <Add />
-                            <Typography>Add</Typography>
-                        </Button>
-                        <Button
-                            color="error"
-                            disabled={!table.getIsSomeRowsSelected()}
-                            variant="contained"
-                        >
-                            <Delete />
-                            <Typography>Delete</Typography>
-                        </Button>
-                    </Box>
-                </Box>
-            );
-        },
+        url: "",
+        handleAddItem: handleAddGenre,
+        handleDeleteItem: handleDeleteGenre,
     });
-    // #endregion
 
     return (
         <Box m="20px">
--- src/pages/admin/series/SeriesAdmin.tsx
+++ src/pages/admin/series/SeriesAdmin.tsx
@@ -1,40 +1,17 @@
-import { Box, Button, ListItemIcon, MenuItem, Typography } from "@mui/material";
+import { Box } from "@mui/material";
 import {
     MaterialReactTable,
-    useMaterialReactTable,
     type MRT_ColumnDef,
-    MRT_GlobalFilterTextField,
-    MRT_ToggleFiltersButton,
-    MRT_ShowHideColumnsButton,
-    MRT_ToggleDensePaddingButton,
-    MRT_ColumnFiltersState,
-    MRT_PaginationState,
-    MRT_SortingState,
 } from "material-react-table";
 import HeaderDashboard from "~/components/admin/headerDashboard/HeaderDashboard";
-import { useState, useEffect, useMemo } from "react";
+import { useMemo } from "react";
 import { useNavigate } from "react-router-dom";
-import { Edit, Delete, Add } from "@mui/icons-material";
 import ISerie from "~/types/ISerie";
-import serieService from "~/services/api/serieService";
-import ISeriesResponse from "~/types/ISeriesResponse";
+import Table from "~/components/admin/table/Table";
 
 const SeriesAdmin = () => {
-    const [rows, setRows] = useState<ISerie[]>([]);
-    const [rowsCount, setRowsCount] = useState<number>(0);
-    const [rowSelection, setRowSelection] = useState<any>({});
-    const [isError, setIsError] = useState(false);
-    const [isLoading, setIsLoading] = useState(false);
-    const [isRefetching, setIsRefetching] = useState(false);
-    const [columnFilters, setColumnFilters] = useState<MRT_ColumnFiltersState>([]);
-    const [globalFilter, setGlobalFilter] = useState("");
-    const [sorting, setSorting] = useState<MRT_SortingState>([]);
-    const [pagination, setPagination] = useState<MRT_PaginationState>({
-        pageIndex: 0,
-        pageSize: 5,
-    });
-
     const navigate = useNavigate();
+
     const columns = useMemo<MRT_ColumnDef<ISerie>[]>(
         () => [
             { accessorKey: "id", header: "Id", enableHiding: true },
@@ -58,201 +35,16 @@ const SeriesAdmin = () => {
         navigate("/admin/series/add");
     }
 
-    async function getSeries(): Promise<void> {
-        if (!rows.length) {
-            setIsLoading(true);
-        } else {
-            setIsRefetching(true);
-        }
-
-        try {
-            const response: ISeriesResponse = await serieService.getSeries({});
-            setRows(response.rows);
-            setRowsCount(response.count);
-        } catch (error) {
-            setIsError(true);
-            console.error(error);
-            return;
-        }
-
-        setIsError(false);
-        setIsLoading(false);
-        setIsRefetching(false);
+    function handleDeleteSerie() {
+        // navigate("/admin/series/add");
     }
 
-    useEffect(() => {
-        getSeries();
-    }, [columnFilters, globalFilter, pagination.pageIndex, pagination.pageSize, sorting]);
-
-    // #region React Material Table logic
-    const table = useMaterialReactTable({
+    const { table } = Table({
         columns,
-        data: rows,
-        getRowId: (row) => String(row.id),
-        enableColumnOrdering: true,
-        enableRowSelection: true,
-        enablePagination: true,
-        enableRowActions: true,
-        enablePinning: true,
-        enableSortingRemoval: true,
-        enableColumnFilterModes: true,
-        enableColumnActions: true,
-        enableColumnFilters: true,
-        enableClickToCopy: true,
-        enableStickyHeader: true,
-        enableStickyFooter: true,
-        manualFiltering: true,
-        manualPagination: true,
-        manualSorting: true,
-        muiToolbarAlertBannerProps: isError
-            ? {
-                  color: "error",
-                  children: "Error loading data",
-              }
-            : undefined,
-        initialState: {
-            columnVisibility: { id: false },
-            showColumnFilters: false,
-            showGlobalFilter: true,
-            showLoadingOverlay: false,
-            density: "compact",
-            columnPinning: {
-                left: ["mrt-row-expand", "mrt-row-select"],
-                right: ["mrt-row-actions"],
-            },
-            columnFilters,
-            globalFilter,
-            isLoading,
-            pagination,
-            showAlertBanner: isError,
-            showProgressBars: isRefetching,
-            sorting,
-        },
-        onColumnFiltersChange: setColumnFilters,
-        onGlobalFilterChange: setGlobalFilter,
-        onPaginationChange: setPagination,
-        onSortingChange: setSorting,
-        onRowSelectionChange: setRowSelection,
-        state: { rowSelection },
-        paginationDisplayMode: "pages",
-        positionToolbarAlertBanner: "bottom",
-        muiSearchTextFieldProps: {
-            size: "small",
-            variant: "outlined",
-        },
-        muiTablePaperProps: {
-            style: {
-                padding: 18,
-            },
-        },
-        // muiTableProps: {
-        //     style: {
-        //         padding: 2
-        //     }
-        // },
-        // muiTableBodyRowProps: {
-        //     style: {
-        //         height: "1rem"
-        //     }
-        // },
-        muiPaginationProps: {
-            color: "secondary",
-            rowsPerPageOptions: [5, 10, 15, 20],
-            shape: "rounded",
-            size: "small",
-            variant: "outlined",
-            style: {
-                paddingTop: 6,
-            },
-        },
-        renderRowActionMenuItems: ({ closeMenu, row }) => [
-            <MenuItem
-                key={0}
-                onClick={() => {
-                    navigate(`/admin/series/${row.original.id}`, {
-                        state: {
-                            userId: row.original.id,
-                            from: "Series",
-                        },
-                    });
-
-                    closeMenu();
-                }}
-                sx={{ m: 0 }}
-            >
-                <ListItemIcon>
-                    <Edit />
-                </ListItemIcon>
-                <Typography>Edit</Typography>
-            </MenuItem>,
-            <MenuItem
-                key={1}
-                onClick={async () => {
-                    // const response = await movieService.updateSerie(row.original, {
-                    //     ...row.original,
-                    //     userIsActive: false,
-                    // });
-
-                    // if (response) {
-                    //     toast.success(CONSTANTS.UPDATE__SUCCESS);
-                    //     getSeries();
-                    // } else {
-                    //     toast.error(CONSTANTS.UPDATE__FAILURE);
-                    // }
-
-                    closeMenu();
-                }}
-                sx={{ m: 0 }}
-            >
-                <ListItemIcon>
-                    <Delete />
-                </ListItemIcon>
-                <Typography>Delete</Typography>
-            </MenuItem>,
-        ],
-        renderTopToolbar: ({ table }) => {
-            return (
-                <Box
-                    sx={() => ({
-                        display: "flex",
-                        gap: "1rem",
-                        p: "10px",
-                        justifyContent: "space-between",
-                    })}
-                >
-                    <Box sx={{ display: "flex", gap: "1rem", alignItems: "center" }}>
-                        <MRT_GlobalFilterTextField table={table} />
-                        <MRT_ToggleFiltersButton table={table} />
-                        <MRT_ShowHideColumnsButton table={table} />
-                        <MRT_ToggleDensePaddingButton table={table} />
-                    </Box>
-                    <Box
-                        sx={{
-                            display: "flex",
-                            placeItems: "center",
-                            placeContent: "center",
-                            gap: "1rem",
-                        }}
-                    >
-                        <Button color="success" onClick={handleAddSerie} variant="contained">
-                            <Add />
-                            <Typography>Add</Typography>
-                        </Button>
-                        <Button
-                            color="error"
-                            disabled={!table.getIsSomeRowsSelected()}
-                            // onClick={handleDeleteUser}
-                            variant="contained"
-                        >
-                            <Delete />
-                            <Typography>Delete</Typography>
-                        </Button>
-                    </Box>
-                </Box>
-            );
-        },
+        url: "",
+        handleAddItem: handleAddSerie,
+        handleDeleteItem: handleDeleteSerie,
     });
-    // #endregion
 
     return (
         <Box m="20px">
