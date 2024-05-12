8d51a3cf853f2c8fe0961bb6b4ec20e05bc9b898
JurgenHasmeta22 <jurgenhasmeta12@yahoo.com>
2024-05-12 16:07:34 +0200

    added reusable logic component for table admin

---
+++ src/components/admin/table/Table.tsx
@@ -0,0 +1,208 @@
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import {
+    MRT_ColumnFiltersState,
+    MRT_GlobalFilterTextField,
+    MRT_ShowHideColumnsButton,
+    MRT_SortingState,
+    MRT_ToggleDensePaddingButton,
+    MRT_ToggleFiltersButton,
+    useMaterialReactTable,
+} from "material-react-table";
+import { Box, Button, ListItemIcon, MenuItem, Typography } from "@mui/material";
+import { Edit, Delete, Add } from "@mui/icons-material";
+import serieService from "~/services/api/serieService";
+
+const Table = ({ columns, url, handleAddItem, handleDeleteItem }: any) => {
+    const [rows, setRows] = useState<any[]>([]);
+    const [rowsCount, setRowsCount] = useState<number>(0);
+    const [rowSelection, setRowSelection] = useState<any>({});
+    const [isError, setIsError] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
+    const [isRefetching, setIsRefetching] = useState(false);
+    const [columnFilters, setColumnFilters] = useState<MRT_ColumnFiltersState>([]);
+    const [globalFilter, setGlobalFilter] = useState("");
+    const [sorting, setSorting] = useState<MRT_SortingState>([]);
+    const [pagination, setPagination] = useState({
+        pageIndex: 0,
+        pageSize: 5,
+    });
+    const navigate = useNavigate();
+
+    const fetchData = async () => {
+        if (!rows.length) {
+            setIsLoading(true);
+        } else {
+            setIsRefetching(true);
+        }
+
+        try {
+            const response = await serieService.getSeries({});
+            setRows(response.rows);
+            setRowsCount(response.count);
+        } catch (error) {
+            setIsError(true);
+            console.error(error);
+        }
+
+        setIsError(false);
+        setIsLoading(false);
+        setIsRefetching(false);
+    };
+
+    useEffect(() => {
+        fetchData();
+    }, [columnFilters, globalFilter, pagination.pageIndex, pagination.pageSize, sorting]);
+
+    const table = useMaterialReactTable({
+        columns,
+        data: rows,
+        getRowId: (row) => String(row.id),
+        enableColumnOrdering: true,
+        enableRowSelection: true,
+        enablePagination: true,
+        enableRowActions: true,
+        enablePinning: true,
+        enableSortingRemoval: true,
+        enableColumnFilterModes: true,
+        enableColumnActions: true,
+        enableColumnFilters: true,
+        enableClickToCopy: true,
+        enableStickyHeader: true,
+        enableStickyFooter: true,
+        manualFiltering: true,
+        manualPagination: true,
+        manualSorting: true,
+        muiToolbarAlertBannerProps: isError
+            ? {
+                  color: "error",
+                  children: "Error loading data",
+              }
+            : undefined,
+        initialState: {
+            columnVisibility: { id: false },
+            showColumnFilters: false,
+            showGlobalFilter: true,
+            showLoadingOverlay: false,
+            density: "compact",
+            columnPinning: {
+                left: ["mrt-row-expand", "mrt-row-select"],
+                right: ["mrt-row-actions"],
+            },
+            columnFilters,
+            globalFilter,
+            isLoading,
+            pagination,
+            showAlertBanner: isError,
+            showProgressBars: isRefetching,
+            sorting,
+        },
+        onColumnFiltersChange: setColumnFilters,
+        onGlobalFilterChange: setGlobalFilter,
+        onPaginationChange: setPagination,
+        onSortingChange: setSorting,
+        onRowSelectionChange: setRowSelection,
+        state: { rowSelection },
+        paginationDisplayMode: "pages",
+        positionToolbarAlertBanner: "bottom",
+        muiSearchTextFieldProps: {
+            size: "small",
+            variant: "outlined",
+        },
+        muiTablePaperProps: {
+            style: {
+                padding: 18,
+            },
+        },
+        muiPaginationProps: {
+            color: "secondary",
+            rowsPerPageOptions: [5, 10, 15, 20],
+            shape: "rounded",
+            size: "small",
+            variant: "outlined",
+            style: {
+                paddingTop: 6,
+            },
+        },
+        renderRowActionMenuItems: ({ closeMenu, row }) => [
+            <MenuItem
+                key={0}
+                onClick={() => {
+                    navigate(`/admin/series/${row.original.id}`, {
+                        state: {
+                            userId: row.original.id,
+                            from: "Series",
+                        },
+                    });
+
+                    closeMenu();
+                }}
+                sx={{ m: 0 }}
+            >
+                <ListItemIcon>
+                    <Edit />
+                </ListItemIcon>
+                <Typography>Edit</Typography>
+            </MenuItem>,
+            <MenuItem
+                key={1}
+                onClick={async () => {
+                    closeMenu();
+                }}
+                sx={{ m: 0 }}
+            >
+                <ListItemIcon>
+                    <Delete />
+                </ListItemIcon>
+                <Typography>Delete</Typography>
+            </MenuItem>,
+        ],
+        renderTopToolbar: ({ table }) => {
+            return (
+                <Box
+                    sx={() => ({
+                        display: "flex",
+                        gap: "1rem",
+                        p: "10px",
+                        justifyContent: "space-between",
+                    })}
+                >
+                    <Box sx={{ display: "flex", gap: "1rem", alignItems: "center" }}>
+                        <MRT_GlobalFilterTextField table={table} />
+                        <MRT_ToggleFiltersButton table={table} />
+                        <MRT_ShowHideColumnsButton table={table} />
+                        <MRT_ToggleDensePaddingButton table={table} />
+                    </Box>
+                    <Box
+                        sx={{
+                            display: "flex",
+                            placeItems: "center",
+                            placeContent: "center",
+                            gap: "1rem",
+                        }}
+                    >
+                        <Button color="success" onClick={handleAddItem} variant="contained">
+                            <Add />
+                            <Typography>Add</Typography>
+                        </Button>
+                        <Button
+                            color="error"
+                            disabled={!table.getIsSomeRowsSelected()}
+                            onClick={handleDeleteItem}
+                            variant="contained"
+                        >
+                            <Delete />
+                            <Typography>Delete</Typography>
+                        </Button>
+                    </Box>
+                </Box>
+            );
+        },
+    });
+
+    return {
+        table,
+    };
+};
+
+export default Table;
--- src/pages/admin/movies/MoviesAdmin.tsx
+++ src/pages/admin/movies/MoviesAdmin.tsx
@@ -1,39 +1,12 @@
-import { Box, Button, ListItemIcon, MenuItem, Typography } from "@mui/material";
-import {
-    MaterialReactTable,
-    useMaterialReactTable,
-    type MRT_ColumnDef,
-    MRT_GlobalFilterTextField,
-    MRT_ToggleFiltersButton,
-    MRT_ShowHideColumnsButton,
-    MRT_ToggleDensePaddingButton,
-    MRT_ColumnFiltersState,
-    MRT_PaginationState,
-    MRT_SortingState,
-} from "material-react-table";
+import { Box } from "@mui/material";
+import { MaterialReactTable, type MRT_ColumnDef } from "material-react-table";
 import HeaderDashboard from "~/components/admin/headerDashboard/HeaderDashboard";
-import { useState, useEffect, useMemo } from "react";
+import { useMemo } from "react";
 import { useNavigate } from "react-router-dom";
-import { Edit, Delete, Add } from "@mui/icons-material";
-import movieService from "~/services/api/movieService";
 import IMovie from "~/types/IMovie";
-import IMoviesResponse from "~/types/IMoviesResponse";
+import Table from "~/components/admin/table/Table";
 
 const MoviesAdmin = () => {
-    const [rows, setRows] = useState<IMovie[]>([]);
-    const [rowsCount, setRowsCount] = useState<number>(0);
-    const [rowSelection, setRowSelection] = useState<any>({});
-    const [isError, setIsError] = useState(false);
-    const [isLoading, setIsLoading] = useState(false);
-    const [isRefetching, setIsRefetching] = useState(false);
-    const [columnFilters, setColumnFilters] = useState<MRT_ColumnFiltersState>([]);
-    const [globalFilter, setGlobalFilter] = useState("");
-    const [sorting, setSorting] = useState<MRT_SortingState>([]);
-    const [pagination, setPagination] = useState<MRT_PaginationState>({
-        pageIndex: 0,
-        pageSize: 5,
-    });
-
     const navigate = useNavigate();
     const columns = useMemo<MRT_ColumnDef<IMovie>[]>(
         () => [
@@ -78,178 +51,16 @@ const MoviesAdmin = () => {
         navigate("/admin/movies/add");
     }
 
-    async function getMovies(): Promise<void> {
-        if (!rows.length) {
-            setIsLoading(true);
-        } else {
-            setIsRefetching(true);
-        }
-
-        try {
-            const response: IMoviesResponse = await movieService.getMovies({});
-            setRows(response.movies);
-            setRowsCount(response.count);
-        } catch (error) {
-            setIsError(true);
-            console.error(error);
-            return;
-        }
-
-        setIsError(false);
-        setIsLoading(false);
-        setIsRefetching(false);
+    function handleDeleteMovie() {
+        // navigate("/admin/movies/add");
     }
 
-    useEffect(() => {
-        getMovies();
-    }, [columnFilters, globalFilter, pagination.pageIndex, pagination.pageSize, sorting]);
-
-    // #region React Material Table logic
-    const table = useMaterialReactTable({
+    const { table } = Table({
         columns,
-        data: rows,
-        getRowId: (row) => String(row.id),
-        enableColumnOrdering: true,
-        enableRowSelection: true,
-        enablePagination: true,
-        enableRowActions: true,
-        enablePinning: true,
-        enableSortingRemoval: true,
-        enableColumnFilterModes: true,
-        enableColumnActions: true,
-        enableColumnFilters: true,
-        enableClickToCopy: true,
-        enableStickyHeader: true,
-        enableStickyFooter: true,
-        manualFiltering: true,
-        manualPagination: true,
-        manualSorting: true,
-        muiToolbarAlertBannerProps: isError
-            ? {
-                  color: "error",
-                  children: "Error loading data",
-              }
-            : undefined,
-        initialState: {
-            columnVisibility: { id: false },
-            showColumnFilters: false,
-            showGlobalFilter: true,
-            showLoadingOverlay: false,
-            density: "compact",
-            columnPinning: {
-                left: ["mrt-row-expand", "mrt-row-select"],
-                right: ["mrt-row-actions"],
-            },
-            columnFilters,
-            globalFilter,
-            isLoading,
-            pagination,
-            showAlertBanner: isError,
-            showProgressBars: isRefetching,
-            sorting,
-        },
-        onColumnFiltersChange: setColumnFilters,
-        onGlobalFilterChange: setGlobalFilter,
-        onPaginationChange: setPagination,
-        onSortingChange: setSorting,
-        onRowSelectionChange: setRowSelection,
-        state: { rowSelection },
-        paginationDisplayMode: "pages",
-        positionToolbarAlertBanner: "bottom",
-        muiSearchTextFieldProps: {
-            size: "small",
-            variant: "outlined",
-        },
-        muiTablePaperProps: {
-            style: {
-                padding: 18,
-            },
-        },
-        muiPaginationProps: {
-            color: "secondary",
-            rowsPerPageOptions: [5, 10, 15, 20],
-            shape: "rounded",
-            size: "small",
-            variant: "outlined",
-            style: {
-                paddingTop: 6,
-            },
-        },
-        renderRowActionMenuItems: ({ closeMenu, row }) => [
-            <MenuItem
-                key={0}
-                onClick={() => {
-                    navigate(`/admin/movies/${row.original.id}`, {
-                        state: {
-                            userId: row.original.id,
-                            from: "Movies",
-                        },
-                    });
-
-                    closeMenu();
-                }}
-                sx={{ m: 0 }}
-            >
-                <ListItemIcon>
-                    <Edit />
-                </ListItemIcon>
-                <Typography>Edit</Typography>
-            </MenuItem>,
-            <MenuItem
-                key={1}
-                onClick={async () => {
-                    closeMenu();
-                }}
-                sx={{ m: 0 }}
-            >
-                <ListItemIcon>
-                    <Delete />
-                </ListItemIcon>
-                <Typography>Delete</Typography>
-            </MenuItem>,
-        ],
-        renderTopToolbar: ({ table }) => {
-            return (
-                <Box
-                    sx={() => ({
-                        display: "flex",
-                        gap: "1rem",
-                        p: "10px",
-                        justifyContent: "space-between",
-                    })}
-                >
-                    <Box sx={{ display: "flex", gap: "1rem", alignItems: "center" }}>
-                        <MRT_GlobalFilterTextField table={table} />
-                        <MRT_ToggleFiltersButton table={table} />
-                        <MRT_ShowHideColumnsButton table={table} />
-                        <MRT_ToggleDensePaddingButton table={table} />
-                    </Box>
-                    <Box
-                        sx={{
-                            display: "flex",
-                            placeItems: "center",
-                            placeContent: "center",
-                            gap: "1rem",
-                        }}
-                    >
-                        <Button color="success" onClick={handleAddMovie} variant="contained">
-                            <Add />
-                            <Typography>Add</Typography>
-                        </Button>
-                        <Button
-                            color="error"
-                            disabled={!table.getIsSomeRowsSelected()}
-                            variant="contained"
-                        >
-                            <Delete />
-                            <Typography>Delete</Typography>
-                        </Button>
-                    </Box>
-                </Box>
-            );
-        },
+        url: "",
+        handleAddItem: handleAddMovie,
+        handleDeleteItem: handleDeleteMovie,
     });
-    // #endregion
 
     return (
         <Box m="20px">
