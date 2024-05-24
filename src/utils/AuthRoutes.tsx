15385064406544cabbe63c48f630a094c7cc8269
JurgenHasmeta22 <jurgenhasmeta12@yahoo.com>
2024-05-24 12:52:05 +0200

    validate bugfix

--- src/utils/AuthRoutes.tsx
+++ src/utils/AuthRoutes.tsx
@@ -1,11 +1,10 @@
-import React from "react";
 import { Outlet, Navigate } from "react-router-dom";
 import { useLocalStorage } from "~/hooks/useLocalStorage";
 
 const AuthRoutes = () => {
     const { getItem } = useLocalStorage<string>("token");
-    
-    return getItem() ? <Navigate to="/movies" /> : <Outlet />;
+
+    return getItem() ? <Navigate to="" /> : <Outlet />;
 };
 
 export default AuthRoutes;
