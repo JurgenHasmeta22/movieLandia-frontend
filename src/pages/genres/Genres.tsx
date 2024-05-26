dc9349925424da59e824499f318b53ceb458f38c
Jurgen Hasmeta <j.hasmeta.ext@teamsystem.com>
2024-05-26 11:19:48 +0200

    hero animated and blurred

--- src/pages/genres/Genres.tsx
+++ src/pages/genres/Genres.tsx
@@ -40,6 +40,7 @@ export default function Genres() {
             <motion.div
                 initial={{ opacity: 0 }}
                 animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
                 transition={{ duration: 0.5 }}
             >
                 <Box
--- src/pages/register/Register.tsx
+++ src/pages/register/Register.tsx
@@ -86,6 +86,7 @@ export default function Register() {
                 <motion.div
                     initial={{ opacity: 0 }}
                     animate={{ opacity: 1 }}
+                    exit={{ opacity: 0 }}
                     transition={{ duration: 0.5 }}
                 >
                     <Box
