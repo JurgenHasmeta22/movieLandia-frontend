ba3bc0e0ee84e8f188b872f57c06b43c7a1cd508
JurgenHasmeta22 <jurgenhasmeta12@yahoo.com>
2024-05-30 13:54:54 +0200

    edit profile

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -5,42 +5,48 @@ import { Outlet } from "react-router-dom";
 import Footer from "~/components/footer/Footer";
 import Header from "~/components/header/Header";
 import ScrollToTop from "~/components/scrollToTop/scrollToTop";
+import { ModalProvider } from "~/services/providers/ModalContext";
+import { RightPanelProvider } from "~/services/providers/RightPanelContext";
 
 const MainLayout = () => {
     return (
-        <Grid container>
-            <Grid item xs={12} paddingTop={8}>
-                <Header />
-                <React.Suspense
-                    fallback={
-                        <Box
-                            sx={{
-                                display: "flex",
-                                placeItems: "center",
-                                placeContent: "center",
-                                height: "100vh",
-                            }}
+        <RightPanelProvider>
+            <ModalProvider>
+                <Grid container>
+                    <Grid item xs={12} paddingTop={8}>
+                        <Header />
+                        <React.Suspense
+                            fallback={
+                                <Box
+                                    sx={{
+                                        display: "flex",
+                                        placeItems: "center",
+                                        placeContent: "center",
+                                        height: "100vh",
+                                    }}
+                                >
+                                    <CircularProgress size={80} thickness={4} color="secondary" />
+                                </Box>
+                            }
                         >
-                            <CircularProgress size={80} thickness={4} color="secondary" />
-                        </Box>
-                    }
-                >
-                    <main>
-                        <motion.div
-                            initial={{ opacity: 0, y: 100 }}
-                            animate={{ opacity: 1, y: 0 }}
-                            exit={{ opacity: 0, y: 100 }}
-                            transition={{ duration: 0.5 }}
-                            style={{ width: "100%" }}
-                        >
-                            <Outlet />
-                        </motion.div>
-                    </main>
-                </React.Suspense>
-                <ScrollToTop />
-                <Footer />
-            </Grid>
-        </Grid>
+                            <main>
+                                <motion.div
+                                    initial={{ opacity: 0, y: 100 }}
+                                    animate={{ opacity: 1, y: 0 }}
+                                    exit={{ opacity: 0, y: 100 }}
+                                    transition={{ duration: 0.5 }}
+                                    style={{ width: "100%" }}
+                                >
+                                    <Outlet />
+                                </motion.div>
+                            </main>
+                        </React.Suspense>
+                        <ScrollToTop />
+                        <Footer />
+                    </Grid>
+                </Grid>
+            </ModalProvider>
+        </RightPanelProvider>
     );
 };
 
--- src/pages/profile/Profile.tsx
+++ src/pages/profile/Profile.tsx
@@ -1,4 +1,13 @@
-import { Box, CircularProgress, Stack, Tab, Tabs, Typography, useTheme } from "@mui/material";
+import {
+    Box,
+    Button,
+    CircularProgress,
+    Stack,
+    Tab,
+    Tabs,
+    Typography,
+    useTheme,
+} from "@mui/material";
 import { useNavigate, useSearchParams } from "react-router-dom";
 import { useStore } from "~/store/store";
 import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
@@ -7,12 +16,30 @@ import TabPanel from "~/components/tab/Tab";
 import SEOHelmet from "~/components/seoHelmet/SEOHelmet";
 import FavoritesTab from "~/components/favorites/Favorites";
 import EmailIcon from "@mui/icons-material/Email";
+import { useCallback, useRef } from "react";
+import { toast } from "react-toastify";
+import userService from "~/services/api/userService";
+import IUser from "~/types/IUser";
+import { useRightPanel } from "~/services/providers/RightPanelContext";
+import * as CONSTANTS from "~/constants/Constants";
+import * as Yup from "yup";
+import ClearAllIcon from "@mui/icons-material/ClearAll";
+import SaveAsIcon from "@mui/icons-material/SaveAs";
+import { FormikProps } from "formik";
+
+const userSchema = Yup.object().shape({
+    userName: Yup.string().required("required"),
+    email: Yup.string().required("required"),
+    password: Yup.string().required("required"),
+});
 
 export default function Profile() {
     const [searchParams, setSearchParams] = useSearchParams();
     const navigate = useNavigate();
-    const { user } = useStore();
+    const { user, setUser } = useStore();
     const theme = useTheme();
+    const { openRightPanel } = useRightPanel();
+    const formikRef = useRef<FormikProps<any>>(null);
     const colors = tokens(theme.palette.mode);
     const tabValue =
         searchParams?.get("tab")! === "favMovies"
@@ -28,6 +55,98 @@ export default function Profile() {
             navigate("/profile?tab=favSeries");
         }
     };
+
+    const handleResetFromParent = () => {
+        formikRef.current?.resetForm();
+    };
+
+    const handleEditProfile = useCallback(() => {
+        openRightPanel({
+            formRef: formikRef,
+            initialValues: {
+                id: user?.id,
+                userName: user?.userName,
+                email: user?.email,
+                password: user?.password,
+            },
+            fields: [
+                {
+                    name: "id",
+                    label: "Id",
+                    variant: "filled",
+                    type: "text",
+                    disabled: true
+                },
+                {
+                    name: "userName",
+                    label: "Username",
+                    variant: "filled",
+                    type: "text",
+                },
+                {
+                    name: "email",
+                    label: "Email",
+                    variant: "filled",
+                    type: "text",
+                },
+                {
+                    name: "password",
+                    label: "Password",
+                    variant: "filled",
+                    type: "password",
+                },
+            ],
+            validationSchema: userSchema,
+            onSave: async (values: any) => {
+                const payload: IUser = {
+                    userName: values.userName,
+                    email: values.email,
+                    password: values.password,
+                };
+                const response = await userService.updateUser(payload, values.userId);
+
+                if (response) {
+                    toast.success(CONSTANTS.UPDATE__SUCCESS);
+                    setUser(response);
+                } else {
+                    toast.error(CONSTANTS.UPDATE__FAILURE);
+                }
+            },
+            title: "Edit Profile",
+            actions: [
+                {
+                    label: CONSTANTS.FORM__RESET__BUTTON,
+                    onClick: () => {
+                        handleResetFromParent();
+                    },
+                    type: "reset",
+                    color: "secondary",
+                    variant: "contained",
+                    sx: {
+                        border: "1px solid #000",
+                        bgcolor: "#ff5252",
+                        fontSize: "15px",
+                        fontWeight: "700",
+                    },
+                    icon: <ClearAllIcon />,
+                },
+                {
+                    label: CONSTANTS.FORM__UPDATE__BUTTON,
+                    type: "submit",
+                    color: "secondary",
+                    variant: "contained",
+                    sx: {
+                        border: "1px solid #000",
+                        bgcolor: "#30969f",
+                        fontSize: "15px",
+                        fontWeight: "700",
+                    },
+                    icon: <SaveAsIcon />,
+                },
+            ],
+            subTitle: "Enter the details of the user you want to edit",
+        });
+    }, [user]);
 
     if (!user) {
         return (
@@ -123,6 +242,17 @@ export default function Profile() {
                         >
                             Favorite Series: {user.favSeries?.length}
                         </Typography>
+                    </Box>
+                    <Box>
+                        <Button
+                            onClick={() => {
+                                handleEditProfile();
+                            }}
+                            color="secondary"
+                            variant="outlined"
+                        >
+                            <Typography component={"span"}>Edit Profile</Typography>
+                        </Button>
                     </Box>
                 </Stack>
                 <Box
