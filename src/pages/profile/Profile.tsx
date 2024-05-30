ea2686ff311bceee3a8e31268497daa9e0078bc4
JurgenHasmeta22 <jurgenhasmeta12@yahoo.com>
2024-05-30 13:25:35 +0200

    profile layout changed

--- src/pages/profile/Profile.tsx
+++ src/pages/profile/Profile.tsx
@@ -1,4 +1,4 @@
-import { Box, CircularProgress, Tab, Tabs, Typography, useTheme } from "@mui/material";
+import { Box, CircularProgress, Stack, Tab, Tabs, Typography, useTheme } from "@mui/material";
 import { useNavigate, useSearchParams } from "react-router-dom";
 import { useStore } from "~/store/store";
 import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
@@ -6,6 +6,7 @@ import { tokens } from "~/utils/theme";
 import TabPanel from "~/components/tab/Tab";
 import SEOHelmet from "~/components/seoHelmet/SEOHelmet";
 import FavoritesTab from "~/components/favorites/Favorites";
+import EmailIcon from "@mui/icons-material/Email";
 
 export default function Profile() {
     const [searchParams, setSearchParams] = useSearchParams();
@@ -52,62 +53,125 @@ export default function Profile() {
                 type="website"
                 canonicalUrl="https://example.com/profile"
             />
-            <Box
-                display={"flex"}
-                flexDirection={"row"}
-                columnGap={1}
-                justifyContent={"center"}
-                alignItems={"center"}
-                mt={8}
-                component={"section"}
-            >
-                <PersonOutlinedIcon color="secondary" fontSize="large" />
-                <Typography color={"secondary"} fontWeight={600} fontSize={18}>
-                    {user.userName}
-                </Typography>
-            </Box>
-            <Box component={"section"} marginTop={4} marginBottom={2}>
-                <Tabs
-                    value={tabValue}
-                    onChange={handleChange}
-                    variant="fullWidth"
-                    orientation="horizontal"
+            <Stack flexDirection="row" width="100%" padding={8} columnGap={4}>
+                <Stack
+                    component="section"
                     sx={{
-                        borderBottom: `1px solid ${colors.greenAccent[800]}`,
+                        width: "30%",
+                        backgroundColor: `${colors.primary[400]}`,
+                        borderRadius: "22px",
+                        padding: 4,
+                        display: "flex",
+                        flexDirection: "column",
+                        boxShadow: 6,
+                        rowGap: 2,
                     }}
                 >
-                    <Tab
-                        label="Favorite Movies"
-                        tabIndex={0}
+                    <Box
                         sx={{
-                            backgroundColor: colors.blueAccent[400],
-                            color: colors.grey[100],
-                            fontWeight: "600",
-                            fontSize: 14,
-                            textTransform: "capitalize",
-                            borderBottom: `${searchParams?.get("tab") === "favMovies" ? "4px solid" : "0px"}`,
+                            display: "flex",
+                            flexDirection: "row",
+                            justifyContent: "start",
+                            alignItems: "center",
                         }}
-                    />
-                    <Tab
-                        label="Favorite Series"
-                        tabIndex={1}
+                    >
+                        <PersonOutlinedIcon color="secondary" fontSize="large" />
+                        <Typography
+                            color="secondary"
+                            fontWeight={500}
+                            fontSize={16}
+                            component={"span"}
+                        >
+                            {user.userName}
+                        </Typography>
+                    </Box>
+                    <Box
                         sx={{
-                            backgroundColor: colors.blueAccent[400],
-                            color: colors.grey[100],
-                            fontWeight: "600",
-                            fontSize: 14,
-                            textTransform: "capitalize",
-                            borderBottom: `${searchParams?.get("tab") === "favSeries" ? "4px solid" : "0px"}`,
+                            display: "flex",
+                            flexDirection: "row",
+                            justifyContent: "start",
+                            alignItems: "center",
+                            columnGap: 0.5,
                         }}
-                    />
-                </Tabs>
-                <TabPanel value={tabValue} index={0} key={0}>
-                    <FavoritesTab type={"Movies"} key={"0"} />
-                </TabPanel>
-                <TabPanel value={tabValue} index={1} key={1}>
-                    <FavoritesTab type={"Series"} key={"1"} />
-                </TabPanel>
-            </Box>
+                    >
+                        <EmailIcon color="secondary" fontSize="large" />
+                        <Typography
+                            color="secondary"
+                            fontWeight={500}
+                            fontSize={16}
+                            component={"span"}
+                        >
+                            {user.email}
+                        </Typography>
+                    </Box>
+                    <Box>
+                        <Typography
+                            color="secondary"
+                            fontWeight={500}
+                            fontSize={16}
+                            component={"span"}
+                        >
+                            Favorite Movies: {user.favMovies?.length}
+                        </Typography>
+                    </Box>
+                    <Box>
+                        <Typography
+                            color="secondary"
+                            fontWeight={500}
+                            fontSize={16}
+                            component={"span"}
+                        >
+                            Favorite Series: {user.favSeries?.length}
+                        </Typography>
+                    </Box>
+                </Stack>
+                <Box
+                    component="section"
+                    sx={{
+                        width: "70%",
+                        backgroundColor: colors.primary[400],
+                        borderRadius: "22px",
+                        padding: 4,
+                        boxShadow: 6,
+                    }}
+                >
+                    <Tabs
+                        value={tabValue}
+                        onChange={handleChange}
+                        variant="fullWidth"
+                        orientation="horizontal"
+                    >
+                        <Tab
+                            label="Favorite Movies"
+                            tabIndex={0}
+                            sx={{
+                                backgroundColor: colors.blueAccent[700],
+                                color: colors.primary[100],
+                                fontWeight: "600",
+                                fontSize: 14,
+                                textTransform: "capitalize",
+                            }}
+                        />
+                        <Tab
+                            label="Favorite Series"
+                            tabIndex={1}
+                            sx={{
+                                backgroundColor: colors.blueAccent[700],
+                                color: colors.primary[100],
+                                fontWeight: "600",
+                                fontSize: 14,
+                                textTransform: "capitalize",
+                            }}
+                        />
+                    </Tabs>
+                    <TabPanel value={tabValue} index={0}>
+                        <FavoritesTab type={"Movies"} />
+                    </TabPanel>
+                    <TabPanel value={tabValue} index={1}>
+                        <FavoritesTab type={"Series"} />
+                    </TabPanel>
+                </Box>
+            </Stack>
         </>
     );
 }
