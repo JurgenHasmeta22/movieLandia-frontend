75916876d736ba9ae813567bc3f59b69bc771ac7
JurgenHasmeta22 <jurgenhasmeta12@yahoo.com>
2024-05-30 12:27:18 +0200

    reusable components fix

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -8,12 +8,12 @@ import { Link } from "react-router-dom";
 import movieService from "~/services/api/movieService";
 import serieService from "~/services/api/serieService";
 import genreService from "~/services/api/genreService";
-import GenreItem from "~/components/genreItem/GenreItem";
 import { motion, useAnimation } from "framer-motion";
 import { useInView } from "react-intersection-observer";
 import CardItem from "~/components/cardItem/CardItem";
 import { useQuery } from "@tanstack/react-query";
 import SEOHelmet from "~/components/seoHelmet/SEOHelmet";
+import ListHomeSection from "./components/ListHomeSection";
 
 const sectionVariants = {
     hidden: { opacity: 0, y: 100 },
@@ -21,7 +21,6 @@ const sectionVariants = {
 };
 
 export default function Home() {
-    // #region Using Tanstack Query to fetch and store data
     const moviesQuery = useQuery({
         queryKey: ["movies"],
         queryFn: () => movieService.getMovies({}),
@@ -46,9 +45,7 @@ export default function Home() {
     const genres: IGenre[] = genresQuery.data?.rows! ?? [];
     const shuffledGenres: IGenre[] = genres.sort(() => Math.random() - 0.5);
     const finalGenres: IGenre[] = shuffledGenres.slice(0, 5);
-    // #endregion
 
-    // #region "Framer Motion stuff to make divs animated"
     const [moviesRef, moviesInView] = useInView({ triggerOnce: true });
     const moviesControls = useAnimation();
 
@@ -75,7 +72,6 @@ export default function Home() {
             genresControls.start("visible");
         }
     }, [genresInView, genresControls]);
-    // #endregion
 
     if (moviesQuery.isLoading || seriesQuery.isLoading || genresQuery.isLoading) {
         return (
@@ -119,123 +115,33 @@ export default function Home() {
             <HomeHeroSection />
             <Container>
                 <Stack flexDirection={"column"} rowGap={10} mb={6} mt={6}>
-                    <motion.div
-                        ref={moviesRef}
-                        animate={moviesControls}
-                        variants={sectionVariants}
-                        transition={{ duration: 0.5 }}
-                        initial="hidden"
-                        style={{ position: "relative" }}
-                    >
-                        <Box
-                            display={"flex"}
-                            flexDirection={"column"}
-                            rowGap={3}
-                            component={"section"}
-                        >
-                            <Link
-                                to="/movies"
-                                style={{
-                                    textDecoration: "none",
-                                    fontWeight: 600,
-                                    fontSize: 18,
-                                    marginLeft: 8,
-                                }}
-                            >
-                                Explore Movies
-                            </Link>
-                            <Stack
-                                direction="row"
-                                flexWrap="wrap"
-                                justifyContent={"center"}
-                                alignContent={"center"}
-                                rowGap={4}
-                                columnGap={4}
-                            >
-                                {finalMovies.map((movie: any) => (
-                                    <CardItem data={movie} key={movie.id} />
-                                ))}
-                            </Stack>
-                        </Box>
-                    </motion.div>
-                    <motion.div
-                        ref={seriesRef}
-                        animate={seriesControls}
-                        variants={sectionVariants}
-                        transition={{ duration: 0.5 }}
-                        initial="hidden"
-                        style={{ position: "relative" }}
-                    >
-                        <Box
-                            display={"flex"}
-                            flexDirection={"column"}
-                            rowGap={3}
-                            component={"section"}
-                        >
-                            <Link
-                                to="/series"
-                                style={{
-                                    textDecoration: "none",
-                                    fontWeight: 600,
-                                    fontSize: 18,
-                                    marginLeft: 8,
-                                }}
-                            >
-                                Explore Series
-                            </Link>
-                            <Stack
-                                direction="row"
-                                flexWrap="wrap"
-                                justifyContent={"center"}
-                                alignContent={"center"}
-                                rowGap={4}
-                                columnGap={4}
-                            >
-                                {finalSeries.map((serie: ISerie) => (
-                                    <CardItem data={serie} type="serie" key={serie.id} />
-                                ))}
-                            </Stack>
-                        </Box>
-                    </motion.div>
-                    <motion.div
-                        ref={genresRef}
-                        animate={genresControls}
-                        variants={sectionVariants}
-                        transition={{ duration: 0.5 }}
-                        initial="hidden"
-                        style={{ position: "relative" }}
-                    >
-                        <Box
-                            display={"flex"}
-                            flexDirection={"column"}
-                            rowGap={3}
-                            component={"section"}
-                        >
-                            <Link
-                                to="/genres"
-                                style={{
-                                    textDecoration: "none",
-                                    fontWeight: 600,
-                                    fontSize: 18,
-                                    marginLeft: 8,
-                                }}
-                            >
-                                Explore Genres
-                            </Link>
-                            <Stack
-                                direction="row"
-                                flexWrap="wrap"
-                                justifyContent={"center"}
-                                alignContent={"center"}
-                                rowGap={4}
-                                columnGap={4}
-                            >
-                                {finalGenres.map((genre: IGenre) => (
-                                    <GenreItem key={genre.id} genre={genre} />
-                                ))}
-                            </Stack>
-                        </Box>
-                    </motion.div>
+                    <ListHomeSection
+                        data={finalMovies}
+                        dataControls={moviesControls}
+                        dataRef={moviesRef}
+                        dataVariants={sectionVariants}
+                        type="movie"
+                        link="/movies"
+                        linkText="Explore Movies"
+                    />
+                    <ListHomeSection
+                        data={finalSeries}
+                        dataControls={seriesControls}
+                        dataRef={seriesRef}
+                        dataVariants={sectionVariants}
+                        type="serie"
+                        link="/series"
+                        linkText="Explore Series"
+                    />
+                    <ListHomeSection
+                        data={finalGenres}
+                        dataControls={genresControls}
+                        dataRef={genresRef}
+                        dataVariants={sectionVariants}
+                        type="genre"
+                        link="/genres"
+                        linkText="Explore Genres"
+                    />
                 </Stack>
             </Container>
         </>
