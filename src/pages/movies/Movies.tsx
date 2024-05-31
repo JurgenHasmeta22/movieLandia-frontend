fb64a23b464b1bb9f32330edd0e7a28f4f16dfcf
JurgenHasmeta22 <jurgenhasmeta12@yahoo.com>
2024-05-31 18:37:39 +0200

    layout fix search

--- src/pages/movies/Movies.tsx
+++ src/pages/movies/Movies.tsx
@@ -131,28 +131,16 @@ export default function Movies() {
                         <Stack
                             display="flex"
                             flexDirection="row"
+                            justifyContent={"space-between"}
                             alignItems="center"
                             component="section"
                         >
-                            <Box
-                                display="flex"
-                                justifyContent="center"
-                                alignItems="center"
-                                sx={{ flexGrow: 1 }}
-                                pl={18}
-                            >
-                                <Typography fontSize={22} color="secondary" variant="h2">
+                            <Box ml={1}>
+                                <Typography fontSize={28} color="secondary" variant="h2">
                                     Movies
                                 </Typography>
                             </Box>
-                            <Box
-                                sx={{
-                                    display: "flex",
-                                    justifyContent: "flex-end",
-                                    alignItems: "center",
-                                    mr: 4,
-                                }}
-                            >
+                            <Box mr={1}>
                                 <SortSelect
                                     sortBy={searchParams.get("sortBy")}
                                     ascOrDesc={searchParams.get("ascOrDesc")}
@@ -162,21 +150,30 @@ export default function Movies() {
                         </Stack>
                     )}
                     {movies.length !== 0 ? (
-                        <Stack
-                            direction="row"
-                            flexWrap="wrap"
-                            justifyContent={"center"}
-                            alignContent={"center"}
-                            rowGap={8}
-                            columnGap={4}
-                            sx={{
-                                marginTop: `${searchParams.get("search") ? 2.5 : 0.2}rem`,
-                            }}
-                        >
-                            {movies.map((movie: IMovie) => (
-                                <CardItem data={movie} key={movie.id} />
-                            ))}
-                        </Stack>
+                        <Box display={"flex"} flexDirection={"column"}>
+                            {searchParams.get("search")! && (
+                                <Box ml={1} mt={4}>
+                                    <Typography fontSize={28} color="secondary" variant="h2">
+                                        Movies
+                                    </Typography>
+                                </Box>
+                            )}
+                            <Stack
+                                direction="row"
+                                flexWrap="wrap"
+                                justifyContent={"flex-start"}
+                                alignContent={"center"}
+                                rowGap={8}
+                                columnGap={4}
+                                sx={{
+                                    marginTop: `${searchParams.get("search") ? 2.5 : 0.2}rem`,
+                                }}
+                            >
+                                {movies.map((movie: IMovie) => (
+                                    <CardItem data={movie} key={movie.id} />
+                                ))}
+                            </Stack>
+                        </Box>
                     ) : (
                         <Box
                             sx={{
@@ -187,7 +184,7 @@ export default function Movies() {
                             }}
                             component={"section"}
                         >
-                            <Typography component={"h1"} fontSize={20} textAlign={"center"}>
+                            <Typography component={"h1"} fontSize={24} textAlign={"center"}>
                                 No Search Result, no movie found with that criteria.
                             </Typography>
                         </Box>
