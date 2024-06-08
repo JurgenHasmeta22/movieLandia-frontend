82a99f457ff301876875e537aca7b93437a45c24
Jurgen Hasmeta <j.hasmeta.ext@teamsystem.com>
2024-06-08 17:25:06 +0200

    spinner color fix

--- src/components/loading/Loading.tsx
+++ src/components/loading/Loading.tsx
@@ -1,7 +1,11 @@
-import { Box, CircularProgress } from "@mui/material";
+import { Box, CircularProgress, useTheme } from "@mui/material";
 import React from "react";
+import { tokens } from "~/utils/theme";
 
 const Loading: React.FC = () => {
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+
     return (
         <Box
             sx={{
@@ -11,7 +15,13 @@ const Loading: React.FC = () => {
                 height: "100vh",
             }}
         >
-            <CircularProgress size={80} thickness={4} color="error" />
+            <CircularProgress
+                size={80}
+                thickness={4}
+                sx={{
+                    color: colors.primary[100],
+                }}
+            />
         </Box>
     );
 };
